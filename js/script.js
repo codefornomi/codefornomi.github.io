f39b0bc928fd5a7a9d3a37fd9ee9db5d75a3617f
ibexorg <chuji@luck.ocn.ne.jp>
2014-10-04 18:37:36 +0900

    インデントを調整

--- js/script.js
+++ js/script.js
@@ -10,212 +10,203 @@ var DistrictModel = function(){
   エリア(ごみ処理の地域）を管理するクラスです。
 */
 var AreaModel = function() {
-  this.district; // ADD #1 地区を追加
-  this.label;
-  this.centerName;
-  this.center;
-  this.trash = new Array();
+	this.district; // ADD #1 地区を追加
+	this.label;
+	this.centerName;
+	this.center;
+	this.trash = new Array();
   /**
-  各ゴミのカテゴリに対して、最も直近の日付を計算します。
-*/
-  this.calcMostRect = function() {
-    for (var i = 0; i < this.trash.length; i++) {
-      this.trash[i].calcMostRect(this);
-    }
-  }
-  /**
-    休止期間（主に年末年始）かどうかを判定します。
-  */
-  this.isBlankDay = function(currentDate) {
-    if (!this.center) {
-        return false;
-    }
-    var period = [this.center.startDate, this.center.endDate];
-
-    if (period[0].getTime() <= currentDate.getTime() &&
-      currentDate.getTime() <= period[1].getTime()) {
-      return true;
-    }
-    return false;
-  }
-  /**
-    ゴミ処理センターを登録します。
-    名前が一致するかどうかで判定を行っております。
-  */
-  this.setCenter = function(center_data) {
-    for (var i in center_data) {
-      if (this.centerName == center_data[i].name) {
-        this.center = center_data[i];
-      }
-    }
-  }
-  /**
-  ゴミのカテゴリのソートを行います。
-*/
-  this.sortTrash = function() {
-    this.trash.sort(function(a, b) {
-      var at = a.mostRecent.getTime();
-      var bt = b.mostRecent.getTime();
-      if (at < bt) return -1;
-      if (at > bt) return 1;
-      return 0;
-    });
-  }
+	 * 各ゴミのカテゴリに対して、最も直近の日付を計算します。
+	 */
+	this.calcMostRect = function() {
+		for (var i = 0; i < this.trash.length; i++) {
+			this.trash[i].calcMostRect(this);
+		}
+	}
+	/**
+	 * 休止期間（主に年末年始）かどうかを判定します。
+	 */
+	this.isBlankDay = function(currentDate) {
+		if (!this.center){
+			return false;
+		}
+		var period = [this.center.startDate, this.center.endDate];
+		if (period[0].getTime() <= currentDate.getTime() 
+				&&  currentDate.getTime() <= period[1].getTime()) {
+			return true;
+		}
+		return false;
+	}
+	/**
+	 * ゴミ処理センターを登録します。
+	 * 名前が一致するかどうかで判定を行っております。
+	 */
+	this.setCenter = function(center_data) {
+		for (var i in center_data) {
+			if (this.centerName == center_data[i].name) {
+				this.center = center_data[i];
+			}
+		}
+	}
+	/**
+	 * ゴミのカテゴリのソートを行います。
+	 */
+	this.sortTrash = function() {
+		this.trash.sort(function(a, b) {
+			var at = a.mostRecent.getTime();
+			var bt = b.mostRecent.getTime();
+			if (at < bt) return -1;
+			if (at > bt) return 1;
+			return 0;
+		});
+	}
 }
 
 /**
   各ゴミのカテゴリを管理するクラスです。
 */
 var TrashModel = function(_lable, _cell, remarks) {
-  this.remarks = remarks;
-  this.dayLabel;
-  this.mostRecent;
-  this.dayList;
-  this.mflag = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-  var monthSplitFlag=_cell.search(/:/)>=0
-  if (monthSplitFlag) {
-    var flag = _cell.split(":");
-    this.dayCell = flag[0].split(" ");
-    var mm = flag[1].split(" ");
-  } else {
-    this.dayCell = _cell.split(" ");
-    var mm = new Array("4", "5", "6", "7", "8", "9", "10", "11", "12", "1", "2", "3");
-  }
-  for (var m in mm) {
-    this.mflag[mm[m] - 1] = 1;
-  }
-  this.label = _lable;
-  this.description;
-  this.regularFlg = 1;      // 定期回収フラグ（デフォルトはオン:1）
-
-  var result_text = "";
-  var today = new Date();
-
-  for (var j in this.dayCell) {
-    if (this.dayCell[j].length == 1) {
-      result_text += "毎週" + this.dayCell[j] + "曜日 ";
-    } else if (this.dayCell[j].length == 2 && this.dayCell[j].substr(0,1) != "*") {
-      result_text += "第" + this.dayCell[j].charAt(1) + this.dayCell[j].charAt(0) + "曜日 ";
-    } else if (this.dayCell[j].length == 2 && this.dayCell[j].substr(0,1) == "*") {
-    } else {
-      // 不定期回収の場合（YYYYMMDD指定）
-      result_text = "不定期 ";
-      this.regularFlg = 0;  // 定期回収フラグオフ
-    }
-  }
-  if (monthSplitFlag){
-    var monthList="";
-    for (var m in this.mflag) {
-      if (this.mflag[m]){
-        if (monthList.length>0){
-          monthList+=","
-        }
-        //mを整数化
-        monthList+=((m-0)+1)
-      }
-    };
-    monthList+="月 "
-    result_text=monthList+result_text
-  }
-  this.dayLabel = result_text;
-
-
-  this.getDateLabel = function() {
-    var result_text = this.mostRecent.getFullYear() + "/" + (1 + this.mostRecent.getMonth()) + "/" + this.mostRecent.getDate();
-    return this.getRemark() + this.dayLabel + " " + result_text;
-  }
+	this.remarks = remarks;
+	this.dayLabel;
+	this.mostRecent;
+	this.dayList;
+	this.mflag = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+	var monthSplitFlag=_cell.search(/:/)>=0
+	if (monthSplitFlag) {
+		var flag = _cell.split(":");
+		this.dayCell = flag[0].split(" ");
+		var mm = flag[1].split(" ");
+	} else {
+		this.dayCell = _cell.split(" ");
+		var mm = new Array("4", "5", "6", "7", "8", "9", "10", "11", "12", "1", "2", "3");
+	}
+	for (var m in mm) {
+		this.mflag[mm[m] - 1] = 1;
+	}
+	this.label = _lable;
+	this.description;
+	this.regularFlg = 1;      // 定期回収フラグ（デフォルトはオン:1）
+	var result_text = "";
+	var today = new Date();
+	
+	for (var j in this.dayCell) {
+		if (this.dayCell[j].length == 1) {
+			result_text += "毎週" + this.dayCell[j] + "曜日 ";
+		} else if (this.dayCell[j].length == 2 && this.dayCell[j].substr(0,1) != "*") {
+			result_text += "第" + this.dayCell[j].charAt(1) + this.dayCell[j].charAt(0) + "曜日 ";
+		} else if (this.dayCell[j].length == 2 && this.dayCell[j].substr(0,1) == "*") {
+		} else {
+			// 不定期回収の場合（YYYYMMDD指定）
+			result_text = "不定期 ";
+			this.regularFlg = 0;  // 定期回収フラグオフ
+		}
+	}
+	if (monthSplitFlag) {
+		var monthList="";
+		for (var m in this.mflag) {
+			if (this.mflag[m]){
+				if (monthList.length>0){
+					monthList+=","
+				}
+				//mを整数化
+				monthList+=((m-0)+1)
+				}
+		};
+		monthList+="月 "
+		result_text=monthList+result_text
+	}
+	this.dayLabel = result_text;
+	
+	this.getDateLabel = function() {
+		var result_text = this.mostRecent.getFullYear() + "/" + (1 + this.mostRecent.getMonth()) + "/" + this.mostRecent.getDate();
+		return this.getRemark() + this.dayLabel + " " + result_text;
+	}
 
   var day_enum = ["日", "月", "火", "水", "木", "金", "土"];
-
+  
   function getDayIndex(str) {
-    for (var i = 0; i < day_enum.length; i++) {
-      if (day_enum[i] == str) {
-        return i;
-      }
-    };
-    return -1;
+	  for (var i = 0; i < day_enum.length; i++) {
+		  if (day_enum[i] == str){
+			  return i;
+		  }
+	  };
+	  return -1;
   }
   /**
    * このごみ収集日が特殊な条件を持っている場合備考を返します。収集日データに"*n" が入っている場合に利用されます
    */
   this.getRemark = function getRemark() {
-    var ret = "";
-    this.dayCell.forEach(function(day){
-      if (day.substr(0,1) == "*") {
-        remarks.forEach(function(remark){
-          if (remark.id == day.substr(1,1)){
-            ret += remark.text + "<br/>";
-          }
-        });
-      };
-    });
-    return ret;
+		  var ret = "";
+		  this.dayCell.forEach(function(day) {
+				  if (day.substr(0,1) == "*") {
+						  remarks.forEach(function(remark) {
+								  if (remark.id == day.substr(1,1)) {
+										  ret += remark.text + "<br/>";
+								  }
+						  });
+				  };
+		  });
+		  return ret;
   }
   /**
   このゴミの年間のゴミの日を計算します。
   センターが休止期間がある場合は、その期間１週間ずらすという実装を行っております。
 */
   this.calcMostRect = function(areaObj) {
-    var day_mix = this.dayCell;
-    var result_text = "";
-    var day_list = new Array();
-
-    // 定期回収の場合
-    if (this.regularFlg == 1) {
-
-      var today = new Date();
-
-      // 12月 +3月　を表現
-      for (var i = 0; i < MaxMonth; i++) {
+		  var day_mix = this.dayCell;
+	  var result_text = "";
+	  var day_list = new Array();
+	  // 定期回収の場合
+	  if (this.regularFlg == 1) {
+		  var today = new Date();
+		  // 12月 +3月　を表現
+		  for (var i = 0; i < MaxMonth; i++) {
 
         var curMonth = today.getMonth() + i;
         var curYear = today.getFullYear() + Math.floor(curMonth / 12);
         var month = (curMonth % 12) + 1;
-
         // 収集が無い月はスキップ
         if (this.mflag[month - 1] == 0) {
-            continue;
+        	continue;
         }
         for (var j in day_mix) {
-          //休止期間だったら、今後一週間ずらす。
-          var isShift = false;
-
-          //week=0が第1週目です。
-          for (var week = 0; week < 5; week++) {
-            //4月1日を起点として第n曜日などを計算する。
-            var date = new Date(curYear, month - 1, 1);
-            var d = new Date(date);
-            //コンストラクタでやろうとするとうまく行かなかった。。
-            //
-            //4月1日を基準にして曜日の差分で時間を戻し、最大５週までの増加させて毎週を表現
-            d.setTime(date.getTime() + 1000 * 60 * 60 * 24 *
-              ((7 + getDayIndex(day_mix[j].charAt(0)) - date.getDay()) % 7) + week * 7 * 24 * 60 * 60 * 1000
-            );
-            //年末年始のずらしの対応
-            //休止期間なら、今後の日程を１週間ずらす
-            if (areaObj.isBlankDay(d)) {
-              if (WeekShift) {
-                isShift = true;
-              } else {
-                continue;
-              }
-            }
-            if (isShift) {
-              d.setTime(d.getTime() + 7 * 24 * 60 * 60 * 1000);
-            }
+        	//休止期間だったら、今後一週間ずらす。
+        	var isShift = false;
+        	//week=0が第1週目です。
+        	for (var week = 0; week < 5; week++) {
+        		//4月1日を起点として第n曜日などを計算する。
+        		var date = new Date(curYear, month - 1, 1);
+        		var d = new Date(date);
+        		//コンストラクタでやろうとするとうまく行かなかった。。
+        		//
+        		//4月1日を基準にして曜日の差分で時間を戻し、最大５週までの増加させて毎週を表現
+        		d.setTime(date.getTime() + 1000 * 60 * 60 * 24 *
+        				((7 + getDayIndex(day_mix[j].charAt(0)) - date.getDay()) % 7) + week * 7 * 24 * 60 * 60 * 1000
+        		);
+        		//年末年始のずらしの対応
+        		//休止期間なら、今後の日程を１週間ずらす
+        		if (areaObj.isBlankDay(d)) {
+        			if (WeekShift) {
+        				isShift = true;
+        			} else {
+        				continue;
+        			}
+        		}
+        		if (isShift) {
+        			d.setTime(d.getTime() + 7 * 24 * 60 * 60 * 1000);
+        		}
             //同じ月の時のみ処理したい
             if (d.getMonth() != (month - 1) % 12) {
               continue;
             }
             //特定の週のみ処理する
             if (day_mix[j].length > 1) {
-              if (week != day_mix[j].charAt(1) - 1) {
-                continue;
-              }
+            	if (week != day_mix[j].charAt(1) - 1) {
+            		continue;
+            	}
             }
-
             day_list.push(d);
-          }
+           }
         }
       }
     } else {
